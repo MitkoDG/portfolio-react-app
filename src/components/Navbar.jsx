7104fbfd482515354a0b2a7b2e2eff16aa0d26f4
MitkoDG <retimid@abv.bg>
2023-02-26 15:24:30 +0200

    added styling for the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
 
 const Navbar = () => {
+  
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
       <div>
         <img src={Logo} alt="Logo" style={{ width: "50px" }} />
       </div>
-      <div>
-        <ul className="flex">
+        <ul className="hidden md:flex">
           <li>Home</li>
           <li>About</li>
           <li>Skills</li>
           <li>Work</li>
           <li>Contact</li>
         </ul>
-      </div>
 
-      <div className="hidden">
+      <div className="md:hidden z-10">
         <FaBars />
       </div>
 
-      <div className="hidden">
+      <div className="hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center">
         <li>Home</li>
         <li>About</li>
         <li>Skills</li>
